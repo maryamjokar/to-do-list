1460f2eed33a4bf39ce08706b8804843826ba90e
MaryamJokar <maryam.79jokar@gmail.com>
2021-11-15 19:17:44 +0330

    scroll and list box

--- to_do_list.py
+++ to_do_list.py
@@ -11,5 +11,14 @@ frame_tasks.pack()
 Listbox_tasks=tkinter.Listbox(root,height=3, width=50)
 Listbox_tasks.pack(side=tkinter.LEFT)
 
+Scrollbar_task = tkinter.Scrollbar(frame_tasks)
+Scrollbar_task.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+
+Listbox_tasks.config(yscrollcommand=Scrollbar_task.set)
+Scrollbar_task.config(command=Listbox_tasks.yview)
+
+entry_task= tkinter.Entry(root,width=50)
+entry_task.pack()
+
 
 root.mainloop()
