3754e15ef1675f85d478c5856918acb5ec600d3c
MaryamJokar <maryam.79jokar@gmail.com>
2021-11-15 19:16:30 +0330

    list box

--- to_do_list.py
+++ to_do_list.py
@@ -8,6 +8,8 @@ root.title("to do list")
 frame_tasks = tkinter.Frame(root)
 frame_tasks.pack()
 
+Listbox_tasks=tkinter.Listbox(root,height=3, width=50)
+Listbox_tasks.pack(side=tkinter.LEFT)
 
 
 root.mainloop()
