b1c516afa0724959aeda801c1cad03121a744860
MaryamJokar <maryam.79jokar@gmail.com>
2021-11-15 19:15:26 +0330

    frame tasks

---
+++ to_do_list.py
@@ -0,0 +1,13 @@
+import tkinter
+import tkinter.messagebox
+
+root = tkinter.Tk()
+root.title("to do list")
+
+#create GUI
+frame_tasks = tkinter.Frame(root)
+frame_tasks.pack()
+
+
+
+root.mainloop()
